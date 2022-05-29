13fab080104f400a40b17252492f0d9573f6cf52
20000706 <67904518+20000706@users.noreply.github.com>
2022-05-29 19:54:54 +1200

    Enemy AI working update
    Enemies can start moving in the dungeon.

--- procedural_dungeon/Assets/script/EnemyAI.cs
+++ procedural_dungeon/Assets/script/EnemyAI.cs
@@ -1,42 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public Transform[] waypoints;
-    int waypointIndex;
-    Vector3 target;
+    public float moveSpeed = 3f;
+    public float rotSpeed = 100f;
+
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        UpdateDestination();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, target) < 1)
+        if (isWandering == false)
         {
-            IterateWaypointIndex();
-            UpdateDestination();
+            StartCoroutine(Wander());
+        }
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+        if (isWalking == true)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
     }
 
-    void UpdateDestination()
+    IEnumerator Wander()
     {
-        target = waypoints[waypointIndex].position;
-        agent.SetDestination(target);
-    }
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 3);
+        int rotateLorR = Random.Range(0, 3);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 5);
 
-    void IterateWaypointIndex()
-    {
-        waypointIndex++;
-        if (waypointIndex == waypoints.Length)
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateLorR == 1)
         {
-            waypointIndex = 0;
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
         }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        isWandering = false;
     }
 }
